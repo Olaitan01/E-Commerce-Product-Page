f347263addf8a57af1aee71e83f10f8143d9ed9a
unknown <abiodunfatunbi4@gmail.com>
2023-02-19 23:57:51 +0100

    updated js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const deductBtn = document.querySelector(".minus");
+/*const deductBtn = document.querySelector(".minus");
 const addBtn = document.querySelector(".add-btn");
 const quantity = document.querySelector(".quantity");
 const addToCartBtn = document.querySelector(".addToCart-btn");
@@ -11,7 +11,7 @@ let cartBtn = document.querySelector(".cartBtn");
 let cartItem = document.querySelector(".cart-item");
 let emptyCart = document.querySelector(".empty-cart");
 const carts = document.querySelector(".carts");
-/**Cart Button */
+
 cartBtn.addEventListener("click", () => {
   if (cartItem.classList.toggle(".active")) {
     cartItem.style.display = "block";
@@ -20,25 +20,25 @@ cartBtn.addEventListener("click", () => {
   }
 });
 
-/**Add Cart Arithmentic */
+
 let quantityEl = parseInt(quantity.innerText);
 let num = 0;
 
 deductBtn.addEventListener("click", reduceQuantity);
 function reduceQuantity() {
   num -= 1;
-  quantity.innerText = quantityEl + num;
+  quantity.innerText =  num;
   
 }
 addBtn.addEventListener("click", increaseQuantity);
 
 function increaseQuantity() {
   num += 1;
-  quantity.innerText = quantityEl + num;
+  quantity.innerText = num;
   
 }
 
-/** Add to cart functionality */
+
 
 addToCartBtn.addEventListener("click", cartAdded);
 
@@ -96,9 +96,8 @@ function cartLists(imgSrc, priceEl, quantityEl, title) {
   
 }
 
-/**update total when quantity is changed */
 
-/**Update total */
+
 function updateTotal() {
   let itemPrice = document.querySelector(".item-price").innerText;
   let price = parseFloat(itemPrice.replace("$", ""));
@@ -108,7 +107,7 @@ function updateTotal() {
   let total = document.querySelector(".total");
   total.innerText = `$${totalEl}`;
 }
-/**Remove Item functionality */
+
 
 function removeItem() {
   let cartRow=document.getElementsByClassName("cart-row")[0]
@@ -119,4 +118,141 @@ function removeItem() {
   let cartNum = document.querySelector(".cart-num");
   num -= 1;
   cartNum.innerText = num;
+}*/
+
+/**Quantity Arithmetic */
+const quantity = document.querySelector(".quantity");
+let num = 0;
+const incrementBtn = document.querySelector(".add-btn");
+incrementBtn.addEventListener("click", increment);
+function increment() {
+  num += 1;
+  quantity.innerText = num;
+  quantityUpdate()
+}
+
+const decrementBtn = document.querySelector(".deduct-btn");
+decrementBtn.addEventListener("click", decrement);
+function decrement() {
+  num -= 1;
+  quantity.innerText = num;
+  quantityUpdate()
+  
+}
+
+/**Show cart item when the cart icon is clicked */
+let cartBtn=document.querySelector(".cartBtn")
+let cartItemEl=document.querySelector(".cart-item")
+cartBtn.addEventListener("click",()=>{
+  if(cartItemEl.classList.toggle("active")){
+    cartItemEl.style.display="block"
+  }else{
+    cartItemEl.style.display="none"
+  }
+})
+
+
+
+/**Add to cart */
+const addToCartBtn = document.querySelector(".addToCart-btn");
+addToCartBtn.addEventListener("click", addToCart);
+function addToCart() {
+  let price = document.querySelector(".price").innerText;
+  let quantity = document.querySelector(".quantity").innerText;
+  let title = document.querySelector(".title").innerText;
+  let imgSrc = document.querySelector("#sneaker-thumbnail").src;
+ 
+  itemAddedToCart(price, quantity, title, imgSrc);
+  updateTotal();
+}
+
+function itemAddedToCart(price, quantity, title, imgSrc) {
+  let cart = document.querySelector(".cart-item");
+  const cartRow = document.createElement("div");
+  cartRow.classList.add(".cart-row")
+  
+  let cartItemQuantityIndication=document.querySelector(".cart-num")
+  let num=0
+  num+=1
+  cartItemQuantityIndication.innerText=num
+  let cartEmpty=document.querySelector(".empty-cart")
+  cartEmpty.innerText=" "
+
+  cartRow.innerHTML = ` <div class="cartRow">
+  <div class="cart-row">
+  
+<span class="item-image"
+  ><img
+    src="${imgSrc}"
+    alt="sneaker image"
+    class="cart-image"
+/></span>
+<div class="item-info">
+  <span class="item-title">${title} </span>
+  <span class="price-calc">
+    <span class="item-price">${price}</span>
+    <span class="multiplication">*</span>
+    <span class="itemQuantity">${quantity}</span>
+  </span>
+  <span class="total">0</span>
+</div>
+
+<button class="remove">
+  <img
+    src="images/icon-delete.svg"
+    alt="delete icon"
+    class="remove-icon"
+  />
+</button>
+
+</div>
+<button class="checkout">Checkout</button>
+</div>
+`;
+
+  
+  cart.append(cartRow);
+  cart.querySelector(".remove").addEventListener("click", remove);
+ 
+}
+
+/**cart number increment */
+function cartQuantityIndicator() {
+  const cartIndicator = document.querySelector(".cart-num");
+  num += 1;
+  cartIndicator.innerText = num;
+}
+
+/**Update total when the add to cart function is clicked */
+
+function updateTotal() {
+  let itemPrice = parseFloat(
+    document.querySelector(".item-price").innerText.replace("$", "")
+  );
+  let itemQuantity = parseInt(
+    document.querySelector(".itemQuantity").innerText
+  );
+  const total = document.querySelector(".total");
+  let totaEl = itemPrice * itemQuantity;
+  total.innerText = `$${totaEl}`;
+}
+
+/**Quantity update */
+function quantityUpdate(){
+  if(isNaN(quantity.innerText)||quantity.innerText<=0){
+    quantity.innerText=1
+    return
+  }
+}
+
+
+/**remove item */
+function remove() {
+  let cartRow = document.querySelector(".cartRow").remove();
+  let cartEmpty=document.querySelector(".empty-cart")
+  cartEmpty.innerText="Your cart is empty"
+  let cartItemQuantityIndication=document.querySelector(".cart-num")
+  
+  num-=1
+  cartItemQuantityIndication.innerText=num
 }
